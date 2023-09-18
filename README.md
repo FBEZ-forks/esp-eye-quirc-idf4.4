4d5a806ad667ec2b9fbd94bc770a8ee4cf983dc8
fbez <francesco.bez@gmail.com>
2023-09-18 14:45:47 +0200

    added qr code picture

--- README.md
+++ README.md
@@ -5,6 +5,36 @@ It uses the [quirc](https://github.com/dlbeer/quirc) libray [idf extra component
 
 This example has been tested on an ESP-EYE with `OV3660` camera.
 
+This repo is to show where the esp32-camera components stops to work as expected.
+
+## Tested ESP32-Camera commits
+
+The qr-code used to test the code is represented below.
+![tested qr code](extra/tested_qr_code.png)
+
+* `86a4951`: working  
+
+### Working log (86a4951)
+```
+I (875) app_peripherals: Camera module is ESP-EYE
+I (875) gpio: GPIO[5]| InputEn: 1| OutputEn: 0| OpenDrain: 0| Pullup: 1| Pulldown: 0| Intr:2 
+I (885) cam_hal: cam init ok
+I (895) sccb: pin_sda 18 pin_scl 23
+I (905) camera: Detected camera at address=0x3c
+I (905) camera: Detected OV3660 camera
+I (905) camera: Camera PID=0x3660 VER=0x00 MIDL=0x00 MIDH=0x00
+I (1225) esp32 ll_cam: node_size: 3840, nodes_per_line: 1, lines_per_node: 4, dma_half_buffer_min:  3840, dma_half_buffer: 15360, lines_per_half_buffer: 16, dma_buffer_size: 30720, image_size: 57600
+I (1225) cam_hal: buffer_size: 30720, half_buffer_size: 15360, node_buffer_size: 3840, node_cnt: 8, total_cnt: 3
+I (1235) cam_hal: Allocating 57600 Byte frame buffer in PSRAM
+I (1245) cam_hal: Allocating 57600 Byte frame buffer in PSRAM
+I (1255) cam_hal: cam config ok
+I (1265) ov3660: Calculated VCO: 160000000 Hz, PLLCLK: 160000000 Hz, SYSCLK: 40000000 Hz, PCLK: 10000000 Hz
+0/1]Data: This is a test for esp32-camera
+0/1] DECODE FAILED: Format data ECC failure
+0/1]Data: This is a test for esp32-camera
+0/1]Data: This is a test for esp32-camera
+```
+
 ## Notes
 
 Please note that there is a minimum distance at which the camera can focus on the qr code. This also put a limit to the minimum side of the qr code. If the qr code is nearer than 5 to 6 cm, it may be necessary to put an additional lens (crf. reference nr. 1).
--- main/app_main.c
+++ main/app_main.c
@@ -10,24 +10,6 @@
 
 static const char *TAG = "APP_CODE_SCANNER";
 
-// esp_err_t rgb565_to_grayscale(uint8_t * grayscale_pixels, uint16_t * rgb565_pixels, size_t size){
-    
-//     uint16_t red = 0;
-//     uint16_t green = 0;
-//     uint16_t blue = 0;
-
-//     for(uint16_t k = 0;k<size/2;k=k+2){
-//         printf("%d] RGB: %d::%04X\n",k,rgb565_pixels[k],rgb565_pixels[k]);
-//         red   = (rgb565_pixels[k] & 0xF800)>>11;
-//         green = (rgb565_pixels[k] & 0x07E0)>>5;
-//         blue  = (rgb565_pixels[k] & 0x001F);
-//         grayscale_pixels[k/2]=(uint8_t)(red/4+3*green/8+blue/16); // approximation of (0.2126 * red) + (0.7152 * green / 2.0) + (0.0722 * blue);
-//         printf("Grayscale: %d::%02X\n",grayscale_pixels[k],grayscale_pixels[k]);
-//     }
-//     return ESP_OK;
-// }
-
-
 static void decode_task()
 {
     if(ESP_OK != app_camera_init()) {
@@ -35,7 +17,6 @@ static void decode_task()
         return;
     }
 
-    
     camera_fb_t *fb = NULL;
 
     // Initializing the quirc handle
@@ -55,11 +36,6 @@ static void decode_task()
     uint16_t p_width = fb->width;
     uint16_t p_height = fb->height;
     uint32_t p_len = fb->len;
-    // printf("w:%d\t, h:%d\t, len:%d\n",p_width,p_height,p_len);
-    // uint8_t nr_bytes = p_len/(p_width*p_height);
-
-    // Buffer where the grayscale picture will be stored
-    //uint8_t * grayscale_buffer = (uint8_t *)malloc(p_len/nr_bytes*sizeof(uint8_t));
 
     if (quirc_resize(q, p_width,p_height) < 0) {
         ESP_LOGE(TAG,"Failed to allocate video  memory\n");
@@ -78,12 +54,10 @@ static void decode_task()
             ESP_LOGI(TAG, "camera get failed\n");
             continue;
         }
-        //printf("Picture Format:%d\nPicture w:%d\th:%d\tlen:%d",fb->format,fb->width,fb->height,fb->len);
         // Decode Progress
     
         memcpy(quirc_begin(q, NULL, NULL), fb->buf, fb->len);
         quirc_end(q);
-        //printf("Detected %d qrcodes\n", quirc_count(q));
 
         num_codes = quirc_count(q);
         for (uint16_t i = 0; i < num_codes; i++) {
@@ -96,17 +70,6 @@ static void decode_task()
             else
                 printf("%d/%d]Data: %s\n",i,num_codes,data.payload);
         }
-        // esp_image_scanner_t *esp_scn = esp_code_scanner_create();
-        // esp_code_scanner_config_t config = {ESP_CODE_SCANNER_MODE_FAST, ESP_CODE_SCANNER_IMAGE_GRAY, fb->width, fb->height};
-        // esp_code_scanner_set_config(esp_scn, config);
-        // int decoded_num = esp_code_scanner_scan_image(esp_scn, fb->buf);
-
-        // if(decoded_num){
-        //     esp_code_scanner_symbol_t result = esp_code_scanner_result(esp_scn);
-        //     printf("{%s}\n",result.data);
-        // }
-        // esp_code_scanner_destroy(esp_scn);
-
 
         esp_camera_fb_return(fb);
         vTaskDelay(10 / portTICK_PERIOD_MS);
